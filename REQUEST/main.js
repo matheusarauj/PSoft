b6e3e22d63404796c61c940ff5aea8a67d501bb5
grabowski74 <matheus.araujo@ccc.ufcg.edu.br>
2019-04-19 18:27:55 -0300

    add babel

--- REQUEST/main.js
+++ REQUEST/main.js
@@ -1,9 +1,11 @@
 
 
-axios.get('https://api.github.com/users/grabowski74')
-    .then(function(response) {
-        console.log(response.data.login);
-    })
-    .catch(function(error) {
-        console.warn(error);
-    });
+class Teste{
+    metodo(){
+
+    }
+
+    outro(){
+        
+    }
+}
