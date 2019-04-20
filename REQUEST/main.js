3772a38b887454274297d9ccd0a4f4b2001a0655
grabowski74 <matheus.araujo@ccc.ufcg.edu.br>
2019-04-20 08:46:43 -0300

    add class

--- REQUEST/main.js
+++ REQUEST/main.js
@@ -1,11 +1,29 @@
-
-
-class Teste{
-    metodo(){
-
+class List {
+    constructor() {
+        this.data = [];
     }
 
-    outro(){
-        
+    add(data){
+        this.data.push(data);
+        console.log(this.data);
     }
+}
+
+class TodoList extends List {
+    constructor(){
+        super();
+
+        this.usuario = "Diego";
+    }
+
+    mostraUsuario(){
+        console.log(this.usuario);
+    }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function() {
+    MinhaLista.add('Novo Todo');
+    MinhaLista.mostraUsuario();
 }
