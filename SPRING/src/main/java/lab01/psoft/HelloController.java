d8933fb4264b73e98adc8ab189af74ee1eef6f4d
Matheus Araújo <39747357+grabowski74@users.noreply.github.com>
2019-05-28 15:34:20 -0300

    add Lab01

---
+++ SPRING/src/main/java/lab01/psoft/HelloController.java
@@ -0,0 +1,20 @@
+package lab01.psoft;
+
+import java.time.LocalTime;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model){
+        model.addAttribute("name", name);
+        model.addAttribute("time", LocalTime.now().getHour() + ":" + LocalTime.now().getMinute());
+        return "hello";
+    }
+
+}
